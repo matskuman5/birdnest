a10a261f504da2133bfea2578b5ca6d5151e408e
Matias <selinmatias5@gmail.com>
2022-12-17 17:14:47 +0200

    install xml-js instead and get drone data from api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const axios = require('axios');
+const convertxml = require('xml-js');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  axios.get('https://assignments.reaktor.com/birdnest/drones').then((xml) => {
+    res.json(convertxml.xml2json(xml.data));
+  });
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
