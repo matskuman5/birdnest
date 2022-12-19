fad1b67cad04e559b8708ad061f5c5edc42d83bf
Matias <selinmatias5@gmail.com>
2022-12-19 19:49:02 +0200

    get data from backend in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/drones').then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <p>Hello!</p>
