cdab94713a67d9ca4250fd384fa600efb714402f
Matias <selinmatias5@gmail.com>
2022-12-23 23:24:43 +0200

    get drones from DB as well

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,53 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const convertxml = require('xml-js');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors());
 app.use(express.static('build'));
 
+mongoose.connect(process.env.MONGODB_URI);
+
+const droneSchema = new mongoose.Schema({
+  serialNumber: String,
+  positionX: Number,
+  positionY: Number,
+});
+
+const Drone = mongoose.model('Drone', droneSchema);
+
 app.get('/drones', (req, res) => {
   axios.get('https://assignments.reaktor.com/birdnest/drones').then((xml) => {
     const data = JSON.parse(convertxml.xml2json(xml.data));
     const droneData = data.elements[0].elements[1];
-    const drones = {
-      snapshotTimestamp: droneData.attributes.snapshotTimestamp,
-      drones: droneData.elements.map((d) => {
-        const drone = {
-          serialNumber: d.elements[0].elements[0].text,
-          model: d.elements[1].elements[0].text,
-          manufacturer: d.elements[2].elements[0].text,
-          mac: d.elements[3].elements[0].text,
-          ipv4: d.elements[4].elements[0].text,
-          ipv6: d.elements[5].elements[0].text,
-          firmware: d.elements[6].elements[0].text,
-          positionY: parseFloat(d.elements[7].elements[0].text),
-          positionX: parseFloat(d.elements[8].elements[0].text),
-          altitude: parseFloat(d.elements[9].elements[0].text),
-        };
-        return drone;
-      }),
-    };
-    res.send(drones);
+    const dronesAPI = droneData.elements.map((d) => {
+      const drone = {
+        serialNumber: d.elements[0].elements[0].text,
+        model: d.elements[1].elements[0].text,
+        manufacturer: d.elements[2].elements[0].text,
+        mac: d.elements[3].elements[0].text,
+        ipv4: d.elements[4].elements[0].text,
+        ipv6: d.elements[5].elements[0].text,
+        firmware: d.elements[6].elements[0].text,
+        positionY: parseFloat(d.elements[7].elements[0].text),
+        positionX: parseFloat(d.elements[8].elements[0].text),
+        altitude: parseFloat(d.elements[9].elements[0].text),
+      };
+      return drone;
+    });
+
+    Drone.find({}).then((dronesDB) => {
+      const final = {
+        snapshotTimestamp: droneData.attributes.snapshotTimestamp,
+        drones: dronesAPI.concat(dronesDB),
+      };
+      res.send(final);
+    });
   });
 });
 
