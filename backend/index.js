e8ac38758a5dd72a0eb1268cd46d312143a5fd03
Matias <selinmatias5@gmail.com>
2022-12-25 01:18:24 +0200

    move violator adding logic into backend

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,95 @@ const violatorSchema = new mongoose.Schema({
 
 const Violator = mongoose.model('Violator', violatorSchema);
 
+var violatorslocal = [];
+var droneslocal = [];
+
+const addViolator = (serialNumber, distance) => {
+  console.log('busted!', serialNumber, distance);
+  axios
+    .get(`https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`)
+    .then((pilot) => {
+      const violator = {
+        firstName: pilot.data.firstName,
+        lastName: pilot.data.lastName,
+        phoneNumber: pilot.data.phoneNumber,
+        email: pilot.data.email,
+        serialNumber: serialNumber,
+        violationTime: Date.now(),
+        closestViolation: distance,
+      };
+      const oldViolator = violatorslocal.find(
+        (p) => p.serialNumber === serialNumber
+      );
+      if (oldViolator === undefined) {
+        violatorslocal.push(violator);
+        //save violator data in mongodb
+        const violatordb = new Violator(violator);
+        violatordb.save();
+      } else {
+        console.log('updating violator');
+        if (violator.closestViolation > oldViolator.closestViolation) {
+          violator.closestViolation = oldViolator.closestViolation;
+        }
+        violatorslocal.map((eviolator) => {
+          if (eviolator.serialNumber === violator.serialNumber) {
+            return violator;
+          }
+          return {
+            ...eviolator,
+          };
+        });
+        Violator.updateOne(
+          { serialNumber: violator.serialNumber },
+          {
+            violationTime: violator.violationTime,
+            closestViolation: violator.closestViolation,
+          }
+        );
+      }
+    });
+};
+
+setInterval(() => {
+  Violator.find({}).then((violators) => {
+    violatorslocals = violators;
+    console.log('amount of violators: ', violatorslocal.length);
+  });
+  axios.get('https://assignments.reaktor.com/birdnest/drones').then((xml) => {
+    const data = JSON.parse(convertxml.xml2json(xml.data));
+    const droneData = data.elements[0].elements[1];
+    const drones = droneData.elements.map((d) => {
+      const drone = {
+        serialNumber: d.elements[0].elements[0].text,
+        model: d.elements[1].elements[0].text,
+        manufacturer: d.elements[2].elements[0].text,
+        mac: d.elements[3].elements[0].text,
+        ipv4: d.elements[4].elements[0].text,
+        ipv6: d.elements[5].elements[0].text,
+        firmware: d.elements[6].elements[0].text,
+        positionY: parseFloat(d.elements[7].elements[0].text),
+        positionX: parseFloat(d.elements[8].elements[0].text),
+        altitude: parseFloat(d.elements[9].elements[0].text),
+      };
+      return drone;
+    });
+    const final = {
+      snapshotTimestamp: droneData.attributes.snapshotTimestamp,
+      drones: drones,
+    };
+    droneslocal = drones;
+    console.log('droneslocal:', droneslocal.length);
+    droneslocal.forEach((drone) => {
+      const distance = Math.sqrt(
+        (drone.positionX - 250000) ** 2 + (drone.positionY - 250000) ** 2
+      );
+      if (distance < 100000) {
+        addViolator(drone.serialNumber, distance);
+      }
+    });
+  });
+}, 2000);
+
 app.get('/drones', (req, res) => {
   axios.get('https://assignments.reaktor.com/birdnest/drones').then((xml) => {
     const data = JSON.parse(convertxml.xml2json(xml.data));
